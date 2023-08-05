c99b078dabdff6810a698ca1fb828261fc241ab5
Shuvrojit Biswas <jsuvro17@gmail.com>
2023-08-05 23:55:06 +0600

    Setup react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,24 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
+import ErrorPage from './components/error-page'
 import './index.css'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider >
-    <App />
+      <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>,
 )
